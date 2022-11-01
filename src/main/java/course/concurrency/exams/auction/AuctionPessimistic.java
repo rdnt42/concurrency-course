a5fce036e25ab969814cbc343cc3f5594788c294
rdnt42 <radiant42@yandex.ru>
2022-11-02 00:04:37 +0300

    добавлена реализация для пессиместичной блокировки

--- src/main/java/course/concurrency/exams/auction/AuctionPessimistic.java
+++ src/main/java/course/concurrency/exams/auction/AuctionPessimistic.java
@@ -3,6 +3,7 @@ package course.concurrency.exams.auction;
 public class AuctionPessimistic implements Auction {
 
     private Notifier notifier;
+    private final Object lock = new Object();
 
     public AuctionPessimistic(Notifier notifier) {
         this.notifier = notifier;
@@ -11,15 +12,31 @@ public class AuctionPessimistic implements Auction {
     private Bid latestBid;
 
     public boolean propose(Bid bid) {
-        if (bid.getPrice() > latestBid.getPrice()) {
-            notifier.sendOutdatedMessage(latestBid);
-            latestBid = bid;
-            return true;
+        if (latestBid == null) {
+            synchronized (lock) {
+                if (latestBid == null) {
+                    latestBid = bid;
+
+                    return true;
+                }
+            }
         }
+
+        if (bid.getPrice() > latestBid.getPrice()) {
+            synchronized (lock) {
+                if (bid.getPrice() > latestBid.getPrice()) {
+                    notifier.sendOutdatedMessage(latestBid);
+                    latestBid = bid;
+
+                    return true;
+                }
+            }
+        }
+
         return false;
     }
 
-    public Bid getLatestBid() {
+    public synchronized Bid getLatestBid() {
         return latestBid;
     }
 }
--- src/main/java/course/concurrency/exams/auction/Notifier.java
+++ src/main/java/course/concurrency/exams/auction/Notifier.java
@@ -1,9 +1,15 @@
 package course.concurrency.exams.auction;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class Notifier {
+    private final ExecutorService executorService = Executors.newFixedThreadPool(72);
 
     public void sendOutdatedMessage(Bid bid) {
-        imitateSending();
+        Runnable task = this::imitateSending;
+
+        executorService.execute(task);
     }
 
     private void imitateSending() {
